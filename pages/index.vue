3620a51a124bef081f8fbad1c4f8bfaaf0d94631
kamalEdvoy <kamal@edvoy.com>
2022-06-16 17:33:28 +0530

    add tailwindcss3

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="max-w-lg p-6 drop-shadow-md shadow-lg shadow-green-500/50">
+      <h2
+        class="text-2xl text-green-600 font-bold underline underline decoration-wavy decoration-blue-500/30 mb-4"
+      >
+        Testing tailwind css3 updated version
+      </h2>
+      <p>
+        I’m Tara Parasd Routray, a full-stack developer based in India. I love
+        to build amazing websites keeping
+        <!-- Utilties used: underline, decoration-sky-500 -->
+        <a class="underline decoration-sky-500">performance and security</a> in
+        mind.
+        <!-- Utilties used: underline, decoration-pink-500 -->
+        Outside of work, I love to
+        <a class="underline decoration-pink-500"
+          >watch Nextflix
+          <!-- Utilties used: underline, decoration-indigo-500 -->
+        </a>
+        and play
+        <a class="underline decoration-indigo-500">outdoor games</a> with my
+        friends.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+});
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
